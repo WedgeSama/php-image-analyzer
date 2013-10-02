3e09f357588c6a6e26a599c0de8e2021a3080c89
WedgeSama <wedgesama@gmail.com>
2013-10-02 17:57:59 +0200

    Begin autoloading of the library

---
+++ lib/Phia/Autoloader.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the Phia package.
+ *
+ * (c) Benjamin Georgeault <https://github.com/WedgeSama/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Phia;
+
+/**
+ * Autoloads Phia classes.
+ *
+ * @author Benjamin Georgeault <wedgesama@gmail.com>
+ */
+class Autoloader {
+}
---
+++ test/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * This file is part of the Phia package.
+ *
+ * (c) Benjamin Georgeault <https://github.com/WedgeSama/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+require_once dirname(__FILE__) . '/../lib/Phia/Autoloader.php';
