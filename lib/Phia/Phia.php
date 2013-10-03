0435bb41b49fcc3bb6a8457a2df87ca849deafb0
WedgeSama <wedgesama@gmail.com>
2013-10-03 17:59:27 +0200

    Add Phia class

---
+++ lib/Phia/Phia.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Phia package.
+ *
+ * (c) Benjamin Georgeault <https://github.com/WedgeSama/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Phia;
+
+class Phia {
+
+    /**
+     * Init Phia with one or more images.
+     *
+     * @param string|array $filename
+     */
+    public function __construct($fileName) {
+    }
+
+    /**
+     * Check if $fileName exist.
+     * Can be a localhost or remote file.
+     *
+     * @param string $fileName
+     * @return boolean
+     */
+    private function checkFile($fileName) {
+    }
+}
